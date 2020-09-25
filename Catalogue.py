bd8c379e26149d24973c439de6f311e716d5844e
Harry Tran <css012013@coderacademy.edu.au>
2020-09-25 20:08:52 +1000

    flake8 hotfixes

--- Catalogue.py
+++ Catalogue.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from Deal import Deal
 
+
 class Catalogue:
     deals = []
 
@@ -17,29 +18,3 @@ class Catalogue:
 
             temp_deal = Deal(title, link, upvote, downvote)
             self.deals.append(temp_deal)
-
-
-
-
-
-# source = requests.get("https://www.ozbargain.com.au/").text
-# soup = BeautifulSoup(source, 'lxml')
-
-# # Deal
-# deal = soup.find(class_="node-ozbdeal")
-
-# # Title
-# title = deal.find(class_="title").a.text
-# print(title)
-
-# # Link
-# link = deal.h2.a["href"]
-# print("https://www.ozbargain.com.au" + link)
-
-# # Upvote
-# upvote = deal.find(class_="nvb voteup").text
-# print(upvote)
-
-# # Downvote
-# downvote = deal.find(class_="nvb votedown").text
-# print(downvote)
