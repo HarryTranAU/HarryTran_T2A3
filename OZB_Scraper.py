898996eba62de4145906ee7f080c4632db2c6a70
Harry Tran <css012013@coderacademy.edu.au>
2020-09-25 18:02:05 +1000

    added OZB_Scraper.py, scrape method

---
+++ OZB_Scraper.py
@@ -0,0 +1,10 @@
+
+import requests
+
+class OZB_Scraper:
+    def __init__(self, url):
+        self.url = url
+
+    def scrape(self):
+        self.source = requests.get(self.url).text
+        return self.source
--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from bs4 import BeautifulSoup
-import requests
+from OZB_Scraper import OZB_Scraper
 
-source = requests.get("https://www.ozbargain.com.au/").text
-soup = BeautifulSoup(source, 'lxml')
+ozb = OZB_Scraper("https://www.ozbargain.com.au/")
 
-# Deal
-deal = soup.find(class_="node-ozbdeal")
-
-# Title
-title = deal.find(class_="title").a.text
-print(title)
-
-# Link
-link = deal.h2.a["href"]
-print("https://www.ozbargain.com.au" + link)
-
-# Upvote
-upvote = deal.find(class_="nvb voteup").text
-print(upvote)
-
-# Downvote
-downvote = deal.find(class_="nvb votedown").text
-print(downvote)
+print(ozb.scrape())
