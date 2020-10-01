1f9bc146d9753c55278063106e4df9abb3769f95
Harry Tran <css012013@coderacademy.edu.au>
2020-10-02 03:32:53 +1000

    updated tests class name

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,11 +1,11 @@
 import unittest
-from Bot import Bot_discord
+from Bot import BotDiscord
 
 
 class TestBot(unittest.TestCase):
 
     def test_validate_url(self):
-        bot_cls = Bot_discord()
+        bot_cls = BotDiscord()
         bot_cls.url = "asdf"
         result = bot_cls.validate_url(bot_cls.url)
         self.assertFalse(result)
